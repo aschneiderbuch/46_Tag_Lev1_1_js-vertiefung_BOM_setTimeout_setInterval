0541bc9b4c525c08324e8a034dbcfa3e3e6feee4
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-15 13:26:38 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ const btn = document.querySelector("#button5Sek")
 function soll5SekWarten() {
     // setTimeout
     let counter = 0;
-    const varZumStopen = setTimeout(() =>{
+    const varZumStopen = setTimeout(() =>{    // Arrow function damit mehr sachen übergeben
         antwortFunction();
         counter = counter +1;
         console.log(counter);
@@ -33,10 +33,23 @@ function soll5SekWarten() {
 
 };
 
+// console.log + ins html
 function antwortFunction() {
     const text = "danke das du 5 Sekunden gewartet hast"
     console.log(text)
-    document.querySelector("body").innerHTML += text;
+
+    // Neues Element erstellen 
+    const antwortElement_Div = document.createElement("div");
+
+    // text mit div verknüpfe --> zuweißen 
+    // bzw. im html erstellen
+antwortElement_Div.innerHTML += text;
+
+    // Antwort Element Div zum body hinzufügen bzw. anzeigen lassen
+    document.querySelector("body").appendChild(antwortElement_Div);
+   
+   // zum css style color red     
+   // kommt jetzt per js    backgroundColor: green hinzu
+    antwortElement_Div.style.backgroundColor = "green";
 };
 
-// console.log + ins html
