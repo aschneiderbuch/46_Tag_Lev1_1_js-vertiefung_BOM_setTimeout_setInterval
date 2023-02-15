e88e94fb285b651a4282dfcd7d8a17340f70e762
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-15 13:17:17 +0100

    clickSetTimeout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
 console.log("test");
+
+/* 
+function die nach x Sekunden Text anzeigt
+
+*/
+
+
+
+const btn = document.querySelector("#button5Sek")
+
+// click
+// ??? :-(  geht nur 1x zu clicken
+    btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    soll5SekWarten();
+    const text = "<br> <br> bitte warte 5 Sekunden <br>"
+    console.log(text)
+    document.querySelector("body").innerHTML += text;
+});
+
+
+// function
+function soll5SekWarten() {
+    // setTimeout
+    let counter = 0;
+    const varZumStopen = setTimeout(() =>{
+        antwortFunction();
+        counter = counter +1;
+        console.log(counter);
+    }, 5000);
+
+
+};
+
+function antwortFunction() {
+    const text = "danke das du 5 Sekunden gewartet hast"
+    console.log(text)
+    document.querySelector("body").innerHTML += text;
+};
+
+// console.log + ins html
